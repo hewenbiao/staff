e58dfa73bfc38417be83afcf08282836157778d6
tanxiongming <tanxiongming@users.noreply.github.com>
2018-06-13 14:35:01 +0800

    ming
    ming

--- html-完善/Network soft aspiration(个人中心欢迎页)/js/eval code(2)/eval code.js
+++
@@ -1,3 +0,0 @@
-({
-	"memberinfo": "<a class=\"sn-login main-color\" href=\"user.php\" target=\"_top\">yiren<\/a><em>\u6b22\u8fce\u60a8\u56de\u6765\uff01<\/em>\r\n<a class=\"sn-register\" href=\"user.php?act=logout\" target=\"_top\">\u9000\u51fa<\/a> \r\n"
-})
